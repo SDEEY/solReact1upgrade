894152de20985263cc50554cc3c58368f2bf95a8
SDEEY <75689630+SDEEY@users.noreply.github.com>
2022-09-11 20:24:08 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -18,10 +18,10 @@ import imgDiscord from './icons8-discord-50.png'
 import imgTwitter from './icons8-twitter-50.png'
 
 
-const solAmount = 0.2
-const image = 'https://pbs.twimg.com/profile_images/1539965747602288641/nehrogmK_400x400.jpg'
-const Title = 'CrocoLabs'
-const supply = 888
+const solAmount = 0
+const image = 'https://pbs.twimg.com/media/Fb89gwLaQAAxE2q?format=png&name=900x900'
+const Title = 'The Misfits NFT'
+const supply = 473
 
 document.title = Title
 document.getElementById('favicon').setAttribute('href', image)
