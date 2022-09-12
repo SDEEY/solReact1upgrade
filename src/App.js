3c2d0adc0733c7213d7f488cece8cfb6652e962f
SDEEY <75689630+SDEEY@users.noreply.github.com>
2022-09-12 20:40:12 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -18,10 +18,10 @@ import imgDiscord from './icons8-discord-50.png'
 import imgTwitter from './icons8-twitter-50.png'
 
 
-const solAmount = 2
-const image = 'https://pbs.twimg.com/profile_images/1557633572177166336/3gGat8Ew_400x400.jpg'
-const Title = 'ANYBODIES'
-const supply = 500
+const solAmount = 0.7
+const image = 'https://pbs.twimg.com/profile_images/1566450861567623170/rOUui4NK_400x400.jpg'
+const Title = 'Solana Vegas Club'
+const supply = 4970
 
 document.title = Title
 document.getElementById('favicon').setAttribute('href', image)
