dfd167b2b13981535be8a49d17f52f368aba81c3
SDEEY <75689630+SDEEY@users.noreply.github.com>
2022-09-14 21:00:33 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ import imgTwitter from './icons8-twitter-50.png'
 const solAmount = 0.2
 const image = 'https://pbs.twimg.com/profile_images/1554778110498930694/_eTNaoq__400x400.jpg'
 const Title = 'The Minions'
-const supply = 1578
+const supply = 3333
 
 document.title = Title
 document.getElementById('favicon').setAttribute('href', image)
