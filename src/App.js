1748c58190a3358e87fb828adf2e65a32d4e9d5c
SDEEY <75689630+SDEEY@users.noreply.github.com>
2022-09-15 15:50:22 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -18,10 +18,10 @@ import imgDiscord from './icons8-discord-50.png'
 import imgTwitter from './icons8-twitter-50.png'
 
 
-const solAmount = 0.777
-const image = 'https://howrare.is/drop_logos/5923_SkWxQpqQ.jpg'
-const Title = 'DoodleSol'
-const supply = 10000
+const solAmount = 1.55
+const image = 'https://pbs.twimg.com/profile_images/1535847247455342592/gujtBM2E_400x400.jpg'
+const Title = 'DefiHoodDAO'
+const supply = 2184
 
 document.title = Title
 document.getElementById('favicon').setAttribute('href', image)
