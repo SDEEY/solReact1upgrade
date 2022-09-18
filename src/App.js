2abc7ac184fe00a272b170b92285027645654d61
SDEEY <75689630+SDEEY@users.noreply.github.com>
2022-09-18 19:52:08 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,10 +6,10 @@ import OwnLayout from "./OwnLayout/OwnLayout";
 import imgDiscord from './icons8-discord-50.png'
 import imgTwitter from './icons8-twitter-50.png'
 
-const solAmount = 0.15
-const image = 'https://pbs.twimg.com/profile_images/1557731937837776897/O-b88IRe_400x400.jpg'
-const Title = 'Beer Bunnies'
-const supply = 1212
+const solAmount = '0.2 / 0.3'
+const image = 'https://media.discordapp.net/attachments/1014879574309470340/1021066051007561748/WhatsApp_Image_2022-09-06_at_17.54.49.jpeg?width=590&height=590'
+const Title = 'Dead Mobs'
+const supply = 2222
 
 document.title = Title
 document.getElementById('favicon').setAttribute('href', image)
