a3c91bbd8453782d991d523633f25516e3883a6d
SDEEY <75689630+SDEEY@users.noreply.github.com>
2022-09-20 04:42:24 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,10 +6,10 @@ import OwnLayout from "./OwnLayout/OwnLayout";
 import imgDiscord from './icons8-discord-50.png'
 import imgTwitter from './icons8-twitter-50.png'
 
-const solAmount = '0.035'
-const image = 'https://pbs.twimg.com/profile_images/1569449901716635654/3YG4g5PH_400x400.jpg'
-const Title = 'Miracle World'
-const supply = 100
+const solAmount = '0'
+const image = 'https://pbs.twimg.com/profile_images/1558601796121202688/svzePfeR_400x400.png'
+const Title = 'Orkes | DAC'
+const supply = 333
 
 document.title = Title
 document.getElementById('favicon').setAttribute('href', image)
