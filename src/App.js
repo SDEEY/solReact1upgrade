c08a4a88c26c2deedb6fe20240a099b19527aee0
SDEEY <75689630+SDEEY@users.noreply.github.com>
2022-09-21 20:25:52 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,10 +6,10 @@ import OwnLayout from "./OwnLayout/OwnLayout";
 import imgDiscord from './icons8-discord-50.png'
 import imgTwitter from './icons8-twitter-50.png'
 
-const solAmount = '0.2'
-const image = 'https://pbs.twimg.com/profile_images/1558433746721939456/CgOslGSM_400x400.jpg'
-const Title = 'DeKeepers'
-const supply = 300
+const solAmount = '0'
+const image = 'https://pbs.twimg.com/profile_images/1476922839865430018/pUYlxfdd_400x400.jpg'
+const Title = 'Pixeledfaces'
+const supply = 500
 
 document.title = Title
 document.getElementById('favicon').setAttribute('href', image)
