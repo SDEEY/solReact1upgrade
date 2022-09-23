f00593a07022d102e2e137810026aceef2c48bf9
SDEEY <75689630+SDEEY@users.noreply.github.com>
2022-09-24 04:53:01 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,10 +6,10 @@ import OwnLayout from "./OwnLayout/OwnLayout";
 import imgDiscord from './icons8-discord-50.png'
 import imgTwitter from './icons8-twitter-50.png'
 
-const solAmount = '0.1'
-const image = 'https://img-cdn.magiceden.dev/rs:fill:640:640:0:0/plain/https://creator-hub-prod.s3.us-east-2.amazonaws.com/old_rossum_pfp_1660888741303.jpeg'
-const Title = "Old Rossum"
-const supply = 222
+const solAmount = '1 / 1.3'
+const image = 'https://pbs.twimg.com/profile_images/1569335077611470851/MUcdT2h4_400x400.jpg'
+const Title = "Bonus Hunter"
+const supply = 505
 
 document.title = Title
 document.getElementById('favicon').setAttribute('href', image)
