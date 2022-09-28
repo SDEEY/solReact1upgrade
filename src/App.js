213e5679b82d24ecdb4b6f5557fe28734618184a
SDEEY <75689630+SDEEY@users.noreply.github.com>
2022-09-28 19:52:27 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,10 +6,10 @@ import OwnLayout from "./OwnLayout/OwnLayout";
 import imgDiscord from './icons8-discord-50.png'
 import imgTwitter from './icons8-twitter-50.png'
 
-const solAmount = '0.8'
-const image = 'https://img-cdn.magiceden.dev/rs:fill:640:640:0:0/plain/https://creator-hub-prod.s3.us-east-2.amazonaws.com/lush_lion_pfp_1661972575684.png'
-const Title = "Lush Safari Club"
-const supply = 10000
+const solAmount = '0'
+const image = 'https://img-cdn.magiceden.dev/rs:fill:640:640:0:0/plain/https://creator-hub-prod.s3.us-east-2.amazonaws.com/monke_eternal_pfp_1664142826118.png'
+const Title = "Monke Eternal"
+const supply = 222
 
 document.title = Title
 document.getElementById('favicon').setAttribute('href', image)
