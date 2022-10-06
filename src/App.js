0658258eb07dc764c564c4012340beea00219ac6
SDEEY <75689630+SDEEY@users.noreply.github.com>
2022-10-06 22:09:38 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,10 +6,10 @@ import OwnLayout from "./OwnLayout/OwnLayout";
 import imgDiscord from './icons8-discord-50.png'
 import imgTwitter from './icons8-twitter-50.png'
 
-const solAmount = '2 / 3'
-const image = 'https://pbs.twimg.com/profile_images/1537417344594718720/LzvDD_22_400x400.jpg'
-const Title = "MetaJewelers"
-const supply = 1500
+const solAmount = '0'
+const image = 'https://pbs.twimg.com/profile_images/1436837757217738752/lcbBH9qI_400x400.jpg'
+const Title = "THE EYEDEAS"
+const supply = 371
 
 document.title = Title
 document.getElementById('favicon').setAttribute('href', image)
