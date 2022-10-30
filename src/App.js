aec7e9d34eaa612043b82a3c7b5c472ac6263a6c
SDEEY <75689630+SDEEY@users.noreply.github.com>
2022-10-30 16:44:49 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,10 +6,10 @@ import OwnLayout from "./OwnLayout/OwnLayout";
 import imgDiscord from './icons8-discord-50.png'
 import imgTwitter from './icons8-twitter-50.png'
 
-const solAmount = '0 / 0.25'
-const image = 'https://img-cdn.magiceden.dev/rs:fill:640:640:0:0/plain/https://creator-hub-prod.s3.us-east-2.amazonaws.com/smcc_pfp_1659984405465.jpeg'
-const Title = "Sol Master Chess Club"
-const supply = 4999
+const solAmount = '0.25'
+const image = 'https://howrare.is/drop_logos/5983_Nta6wYG7.jpg'
+const Title = "RolandStudios"
+const supply = 3333
 
 document.title = Title
 document.getElementById('favicon').setAttribute('href', image)
