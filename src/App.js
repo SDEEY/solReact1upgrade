f339ad598a3bacc20e78a3287f19a6fed7f149fc
SDEEY <75689630+SDEEY@users.noreply.github.com>
2022-11-24 01:58:49 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,10 +6,10 @@ import OwnLayout from "./OwnLayout/OwnLayout";
 import imgDiscord from './icons8-discord-50.png'
 import imgTwitter from './icons8-twitter-50.png'
 
-const solAmount = '0'
-const image = 'https://pbs.twimg.com/profile_images/1581949240267472897/6MwLOZay_400x400.png'
-const Title = "MATCHDAY"
-const supply = 5071
+const solAmount = '3'
+const image = 'https://pbs.twimg.com/profile_images/1521110199972642817/SF1v6HVF_400x400.jpg'
+const Title = "MIZUSHI DRAGONS CLUB"
+const supply = 555
 
 document.title = Title
 document.getElementById('favicon').setAttribute('href', image)
