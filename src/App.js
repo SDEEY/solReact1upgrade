e371b4f5c609fcd2c1016c38b3edf42f65fea411
SDEEY <75689630+SDEEY@users.noreply.github.com>
2022-12-08 18:19:53 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,10 +6,10 @@ import OwnLayout from "./OwnLayout/OwnLayout";
 import imgDiscord from './icons8-discord-50.png'
 import imgTwitter from './icons8-twitter-50.png'
 
-const solAmount = '0.49 / 0.61'
-const image = 'https://media.discordapp.net/attachments/1030197923406950430/1046754020599017562/just_flip.png?width=610&height=610'
-const Title = "JUSTFLIP"
-const supply = 161
+const solAmount = '0.25'
+const image = 'https://media.discordapp.net/attachments/1002610420651143199/1050038649783332904/deff_giff_comp.gif?width=610&height=610'
+const Title = "FUGIAT"
+const supply = 971
 
 document.title = Title
 document.getElementById('favicon').setAttribute('href', image)
