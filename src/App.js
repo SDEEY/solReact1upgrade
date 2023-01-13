172045f94418f9d3e2951a552d5abd3cb5c43f1a
SDEEY <75689630+SDEEY@users.noreply.github.com>
2023-01-13 23:47:53 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,10 +6,10 @@ import OwnLayout from "./OwnLayout/OwnLayout";
 import imgDiscord from './icons8-discord-50.png'
 import imgTwitter from './icons8-twitter-50.png'
 
-const solAmount = '0 / 0.2'
-const image = 'https://pbs.twimg.com/profile_images/1598242999712059393/ihF_6Fzi_400x400.jpg'
-const Title = "MOON ROLL"
-const supply = 127
+const solAmount = '0'
+const image = 'https://pbs.twimg.com/profile_images/1589329568053207043/EWyQVR0B_400x400.jpg'
+const Title = "DEMAGIC ISLAND - THE HANGMAN"
+const supply = 100
 
 document.title = Title
 document.getElementById('favicon').setAttribute('href', image)
