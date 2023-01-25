1e482d956d609d5817dc9fbf660faa543c1d711b
SDEEY <75689630+SDEEY@users.noreply.github.com>
2023-01-25 22:48:28 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,10 +6,10 @@ import OwnLayout from "./OwnLayout/OwnLayout";
 import imgDiscord from './icons8-discord-50.png'
 import imgTwitter from './icons8-twitter-50.png'
 
-const solAmount = '0.55 / 0.7 / 0.85'
-const image = 'https://pbs.twimg.com/profile_images/1579191769094189056/63dK2orX_400x400.jpg'
-const Title = "FILTH LABS"
-const supply = 971
+const solAmount = '0'
+const image = 'https://media.discordapp.net/attachments/1058881784328245339/1067450950345834496/571.png?width=587&height=587'
+const Title = "DABLOONS"
+const supply = 10000
 
 document.title = Title
 document.getElementById('favicon').setAttribute('href', image)
