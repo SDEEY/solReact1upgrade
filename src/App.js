2184e63faddea68dbff4ea86df31b1ee3f66f372
SDEEY <75689630+SDEEY@users.noreply.github.com>
2023-03-04 20:14:02 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,10 +6,10 @@ import OwnLayout from "./OwnLayout/OwnLayout";
 import imgDiscord from './icons8-discord-50.png'
 import imgTwitter from './icons8-twitter-50.png'
 
-const solAmount = '0'
-const image = 'https://pbs.twimg.com/profile_images/1627884662059139072/7wS10FAT_400x400.jpg'
-const Title = "DEGEN DICKS"
-const supply = 42
+const solAmount = '0.069'
+const image = 'https://pbs.twimg.com/profile_images/1622028134261428224/m1umlVUL_400x400.jpg'
+const Title = "MARAUDERS"
+const supply = 2222
 
 document.title = Title
 document.getElementById('favicon').setAttribute('href', image)
