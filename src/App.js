ff9be907d07bc1c0d69605ff1746bf53f9277caa
SDEEY <75689630+SDEEY@users.noreply.github.com>
2023-03-17 01:56:39 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,10 +6,10 @@ import OwnLayout from "./OwnLayout/OwnLayout";
 import imgDiscord from './icons8-discord-50.png'
 import imgTwitter from './icons8-twitter-50.png'
 
-const solAmount = '0.09'
-const image = 'https://pbs.twimg.com/profile_images/1573259708441436163/KroZkFPV_400x400.jpg'
-const Title = "HLabs"
-const supply = 177
+const solAmount = '0.15'
+const image = 'https://pbs.twimg.com/profile_images/1584624536191909890/CpmdsXd7_400x400.jpg'
+const Title = "The Soltoshis"
+const supply = 1500
 
 document.title = Title
 document.getElementById('favicon').setAttribute('href', image)
