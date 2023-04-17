be6b926ae48c91c87e00fe61a780330fcdf3e7c2
SDEEY <75689630+SDEEY@users.noreply.github.com>
2023-04-17 20:55:04 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,10 +6,10 @@ import OwnLayout from "./OwnLayout/OwnLayout";
 import imgDiscord from './icons8-discord-50.png'
 import imgTwitter from './icons8-twitter-50.png'
 
-const solAmount = '0.08'
-const image = 'https://pbs.twimg.com/profile_images/1647182842630139904/2NGIc4QE_400x400.jpg'
-const Title = 'Sleepy Lionz'
-const supply = 99
+const solAmount = '0.369'
+const image = 'https://pbs.twimg.com/profile_images/1585028096486514706/K_7fQj2e_400x400.jpg'
+const Title = 'Tribe Dragonz'
+const supply = 685
 
 document.title = Title
 document.getElementById('favicon').setAttribute('href', image)
