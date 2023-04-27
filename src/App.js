1ff638e3ff7fc34690f37c3ad9bc89db32a9d00f
SDEEY <75689630+SDEEY@users.noreply.github.com>
2023-04-27 21:54:39 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,9 +6,9 @@ import OwnLayout from "./OwnLayout/OwnLayout";
 import imgDiscord from './icons8-discord-50.png'
 import imgTwitter from './icons8-twitter-50.png'
 
-const solAmount = '0'
+const solAmount = '5'
 const image = 'https://pbs.twimg.com/profile_images/1629988094958006272/NibBcPcZ_400x400.jpg'
-const Title = 'Icelolly Labz'
+const Title = 'raposa'
 const supply = 100
 
 document.title = Title
