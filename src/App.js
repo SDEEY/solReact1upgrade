b55bed44872a312e33bacdad91ae006967c18f58
SDEEY <75689630+SDEEY@users.noreply.github.com>
2023-04-29 00:06:52 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,10 +6,10 @@ import OwnLayout from "./OwnLayout/OwnLayout";
 import imgDiscord from './icons8-discord-50.png'
 import imgTwitter from './icons8-twitter-50.png'
 
-const solAmount = '1.2'
-const image = 'https://pbs.twimg.com/profile_images/1635300620524740612/f0gQw6gk_400x400.jpg'
-const Title = 'Zorath.'
-const supply = 3131
+const solAmount = '0'
+const image = 'https://pbs.twimg.com/profile_images/1644108455332937728/gZm81LLq_400x400.jpg'
+const Title = 'YDSP!'
+const supply = 5555
 
 document.title = Title
 document.getElementById('favicon').setAttribute('href', image)
