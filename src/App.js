99d480134e8011ddc4282ed6135742061f46a454
SDEEY <75689630+SDEEY@users.noreply.github.com>
2023-06-17 19:07:59 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,10 +6,10 @@ import OwnLayout from "./OwnLayout/OwnLayout";
 import imgDiscord from './icons8-discord-50.png'
 import imgTwitter from './icons8-twitter-50.png'
 
-const solAmount = '0.3/0.5'
-const image = 'https://pbs.twimg.com/media/FyxPOFBakAICFWD?format=jpg&name=medium'
-const Title = 'Solana Hounds'
-const supply = 1271
+const solAmount = '0.79/0.95'
+const image = 'https://pbs.twimg.com/profile_images/1645037454645243906/hP8FLVPP_400x400.jpg'
+const Title = 'Saiko Samurai'
+const supply = 191
 
 document.title = Title
 document.getElementById('favicon').setAttribute('href', image)
