6ce880bd29032a0a51e0c4c1ae8633ca5b4cef04
SDEEY <75689630+SDEEY@users.noreply.github.com>
2023-07-22 20:18:43 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,10 +6,10 @@ import OwnLayout from "./OwnLayout/OwnLayout";
 import imgDiscord from './icons8-discord-50.png'
 import imgTwitter from './icons8-twitter-50.png'
 
-const solAmount = '3/4/4.5/5'
-const image = 'https://pbs.twimg.com/profile_images/1680587144308441090/q1Z33X8l_400x400.jpg'
-const Title = 'FomoFrankie.com'
-const supply = 671
+const solAmount = '0.19/0.35'
+const image = 'https://pbs.twimg.com/profile_images/1678045937061490689/Z13anMMa_400x400.jpg'
+const Title = 'BF Murderer'
+const supply = 71
 
 document.title = Title
 document.getElementById('favicon').setAttribute('href', image)
