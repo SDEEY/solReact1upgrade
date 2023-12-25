eb71d4908ad88557643218bb28d3405e774d62d0
SDEEY <75689630+SDEEY@users.noreply.github.com>
2023-12-25 15:31:28 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -364,10 +364,11 @@ function App() {
             // NODE, {
             // confirmTransactionInitialTimeout: 60 * 1000,
             //clusterApiUrl('mainnet-beta'),
-            'https://api.mainnet-beta.solana.com',
+            //'https://api.mainnet-beta.solana.com',
             //'https://solana-api.projectserum.com/',
             //'https://solana-api.syndica.io/access-token/eBEyTot5KvUDoVsrvIGPzGjTuTfPOnGyXes6oprbhADlNK2fjMhHH56hoQw1NFOA/rpc',
             //'https://solana-api.syndica.io/access-token/pYZcv9VHe1vUNnhvQ7zAnXbwbkgbo1MGuHcMNNOwu0lQoQhoT6EKcPyrpVLZceJA/rpc',
+            'https://svc.blockdaemon.com/solana/mainnet/native',
             'confirmed'
             // }
         );
