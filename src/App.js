e9a6dfccf69a8199c2461b72bd9905c7ee17c1ea
SDEEY <75689630+SDEEY@users.noreply.github.com>
2024-01-02 20:48:52 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,10 +6,10 @@ import OwnLayout from "./OwnLayout/OwnLayout";
 import imgDiscord from './icons8-discord-50.png'
 import imgTwitter from './icons8-twitter-50.png'
 
-const solAmount = '0'
-const image = 'https://pbs.twimg.com/profile_images/1739706653652529152/QOxhTCNR_400x400.jpg'
-const Title = 'Dumbland'
-const supply = 155
+const solAmount = '0.25'
+const image = 'https://pbs.twimg.com/profile_images/1737802924951007232/u9jxyuzn_400x400.jpg'
+const Title = 'Bluz Degen Club'
+const supply = 844
 
 document.title = Title
 document.getElementById('favicon').setAttribute('href', image)
