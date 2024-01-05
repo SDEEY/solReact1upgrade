fd46bafe12a22bbb487b8ddc1dfc34bfce0dc31c
SDEEY <75689630+SDEEY@users.noreply.github.com>
2024-01-05 17:09:05 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,10 +6,10 @@ import OwnLayout from "./OwnLayout/OwnLayout";
 import imgDiscord from './icons8-discord-50.png'
 import imgTwitter from './icons8-twitter-50.png'
 
-const solAmount = '0.2'
-const image = 'https://pbs.twimg.com/profile_images/1741936002812272640/BcdOzZzK_400x400.jpg'
-const Title = 'Mad Ghosts'
-const supply = 16
+const solAmount = '0.25'
+const image = 'https://pbs.twimg.com/profile_images/1742143254848471040/-VPCh9aM_400x400.jpg'
+const Title = 'Celestial Kids '
+const supply = 75
 
 document.title = Title
 document.getElementById('favicon').setAttribute('href', image)
