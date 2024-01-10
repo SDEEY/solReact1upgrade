2d6ea07b341c2aa353c9abc86af1c293824e9e0e
SDEEY <75689630+SDEEY@users.noreply.github.com>
2024-01-10 17:28:06 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,10 +6,10 @@ import OwnLayout from "./OwnLayout/OwnLayout";
 import imgDiscord from './icons8-discord-50.png'
 import imgTwitter from './icons8-twitter-50.png'
 
-const solAmount = '0.1'
-const image = 'https://pbs.twimg.com/profile_images/1742351654182096896/4hjrlGxA_400x400.jpg'
-const Title = 'DeBears'
-const supply = 77
+const solAmount = '0.069'
+const image = 'https://pbs.twimg.com/profile_images/1738923304092856320/7qVzhjOa_400x400.jpg'
+const Title = 'The Newts'
+const supply = 1469
 
 document.title = Title
 document.getElementById('favicon').setAttribute('href', image)
