df2b40a2163fbc4c352b9e16af7887a9bb1839ea
SDEEY <75689630+SDEEY@users.noreply.github.com>
2024-01-11 21:58:01 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,10 +6,10 @@ import OwnLayout from "./OwnLayout/OwnLayout";
 import imgDiscord from './icons8-discord-50.png'
 import imgTwitter from './icons8-twitter-50.png'
 
-const solAmount = '0.125'
-const image = 'https://pbs.twimg.com/profile_images/1738241455934648320/prJVEQKf_400x400.jpg'
-const Title = 'Elders'
-const supply = 255
+const solAmount = '0.069'
+const image = 'https://pbs.twimg.com/profile_images/1739942552504324096/5mbkCwJO_400x400.jpg'
+const Title = '250 Days Of Drump'
+const supply = 250
 
 document.title = Title
 document.getElementById('favicon').setAttribute('href', image)
