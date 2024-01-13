eda79533e1d41c8dc90fb18eedc2846c89d2f97d
SDEEY <75689630+SDEEY@users.noreply.github.com>
2024-01-13 22:47:57 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,10 +6,10 @@ import OwnLayout from "./OwnLayout/OwnLayout";
 import imgDiscord from './icons8-discord-50.png'
 import imgTwitter from './icons8-twitter-50.png'
 
-const solAmount = '0.06'
-const image = 'https://pbs.twimg.com/profile_images/1723996932035457024/LF7iAB-6_400x400.jpg'
-const Title = 'Amai Aki'
-const supply = 480
+const solAmount = '0.089'
+const image = 'https://pbs.twimg.com/profile_images/1742913015090151424/p_xSGulU_400x400.jpg'
+const Title = 'Dracarys'
+const supply = 355
 
 document.title = Title
 document.getElementById('favicon').setAttribute('href', image)
