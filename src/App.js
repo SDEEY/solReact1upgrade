9ead1c07e635804717656d06a826f816444fa3c3
SDEEY <75689630+SDEEY@users.noreply.github.com>
2024-01-13 20:24:05 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,10 +6,10 @@ import OwnLayout from "./OwnLayout/OwnLayout";
 import imgDiscord from './icons8-discord-50.png'
 import imgTwitter from './icons8-twitter-50.png'
 
-const solAmount = '0.2'
-const image = 'https://pbs.twimg.com/profile_images/1740032526326923264/vDdrZkAT_400x400.jpg'
-const Title = 'JUPY CATS'
-const supply = 190
+const solAmount = '0.12'
+const image = 'https://pbs.twimg.com/profile_images/1744794955011231744/k4f7OsGk_400x400.png'
+const Title = 'The Faceless'
+const supply = 42
 
 document.title = Title
 document.getElementById('favicon').setAttribute('href', image)
