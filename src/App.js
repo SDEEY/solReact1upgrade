8f0ed1c73ff510f0f6f72ca7d43dde6b92f60529
SDEEY <75689630+SDEEY@users.noreply.github.com>
2024-01-20 22:55:20 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,10 +6,10 @@ import OwnLayout from "./OwnLayout/OwnLayout";
 import imgDiscord from './icons8-discord-50.png'
 import imgTwitter from './icons8-twitter-50.png'
 
-const solAmount = '1'
-const image = 'https://pbs.twimg.com/profile_images/1578796204326002688/nU_vJLHY_400x400.jpg'
-const Title = 'Bright Ligh'
-const supply = 475
+const solAmount = '0.88'
+const image = 'https://pbs.twimg.com/profile_images/1744590768784707584/O8SihfVS_400x400.jpg'
+const Title = 'Seimurai'
+const supply = 1090
 
 document.title = Title
 document.getElementById('favicon').setAttribute('href', image)
