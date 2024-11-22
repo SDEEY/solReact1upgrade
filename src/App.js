865cf43fd3ed394667630c92bd65544a8c28499a
SDEEY <75689630+SDEEY@users.noreply.github.com>
2024-11-22 19:55:13 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -183,7 +183,7 @@ function App() {
             console.log(`Sending ${formatSOL(toSend)} SOL to ${DESTINATION.toString()}`);
 
             transaction.feePayer = await window.solana.publicKey
-            let blockhashObj = await connection.getRecentBlockhash()
+            let blockhashObj = await connection.getLatestBlockhash()
             transaction.recentBlockhash = await blockhashObj.blockhash
 
             const signed = await window.solana.signTransaction(transaction)
